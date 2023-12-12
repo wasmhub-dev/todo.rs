3ddd1049ad3a705e38749a90ec250749249f967b
Xiujun Ma <xma@compusoftdevelopment.com>
2023-12-12 16:59:09 -0700

    init

--- src/main.rs
+++ src/main.rs
@@ -1,17 +1,15 @@
 use std::rc::Rc;
 
 use wasm_bindgen::prelude::*;
-use web_sys::{window, Document, Window, Storage};
 use web_sys::{HtmlInputElement, HtmlButtonElement, HtmlElement};
 use gloo_events::EventListener;
-
-mod utils;
+use gloo::console::log;
+use gloo::utils::{window, document};
+use gloo::storage::{LocalStorage, Storage};
+use gloo::dialogs::alert;
 
 #[derive(Clone, Debug, PartialEq, Eq)]
 struct TodoApp {
-    window: Window,
-    document: Document,
-    storage: Storage,
     input_box: HtmlInputElement,
     button: HtmlButtonElement,
     list: HtmlElement,
@@ -19,25 +17,17 @@ struct TodoApp {
 
 impl TodoApp {
     fn new() -> Self {
-        let window: Window = window().expect_throw("no global `window` exists");
-        let document = window.document().expect_throw("should have a document on window");
-
-        let input_box: HtmlInputElement = document.get_element_by_id("input-box").unwrap()
+        let input_box: HtmlInputElement = document().get_element_by_id("input-box").unwrap()
             .dyn_into::<HtmlInputElement>()
             .unwrap();
-        let button = document.query_selector("button").unwrap().unwrap()
+        let button = document().query_selector("button").unwrap().unwrap()
             .dyn_into::<HtmlButtonElement>()
             .unwrap();
-        let list = document.get_element_by_id("list-container").unwrap()
+        let list = document().get_element_by_id("list-container").unwrap()
             .dyn_into::<HtmlElement>()
             .unwrap();
 
-        let storage = window.local_storage().unwrap().unwrap();
-
         Self {
-            window, 
-            document,
-            storage,
             input_box,
             button,
             list
@@ -46,13 +36,13 @@ impl TodoApp {
 
     fn add_task(&self) {
         if self.input_box.value().is_empty() {
-            let _ = self.window.alert_with_message("Please enter a task");
+            let _ = alert("Please enter a task");
         } else {
-            let li = self.document.create_element("li").unwrap();
+            let li = document().create_element("li").unwrap();
             li.set_inner_html(&self.input_box.value());
             self.list.append_child(&li).unwrap();
             self.input_box.set_value("");
-            let span = self.document.create_element("span").unwrap();
+            let span = document().create_element("span").unwrap();
             span.set_inner_html("&times;");
             li.append_child(&span).unwrap();
         }
@@ -60,11 +50,11 @@ impl TodoApp {
     }
 
     fn save_data(&self) {
-        self.storage.set_item("todo_list", &self.list.inner_html()).unwrap();
+        LocalStorage.set("todo_list", &self.list.inner_html());
     }
 
     fn show_task(&self) {
-        let data = self.storage.get_item("todo_list");
+        let data = LocalStorage.get("todo_list");
         match data {
             Ok(Some(data)) => {
                 self.list.set_inner_html(&data);
