63855104c1332f36be5ea98f4233d4fe10527ce1
Xiujun Ma <xma@compusoftdevelopment.com>
2023-12-13 16:38:53 -0700

    init

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,5 @@
 mod state;
 
-use std::rc::Rc;
-
 use state::{State, Task};
 use wasm_bindgen::prelude::*;
 use web_sys::{HtmlInputElement, HtmlButtonElement, HtmlElement};
@@ -34,27 +32,44 @@ impl TodoApp {
         let saved_json: String = LocalStorage::get("todo").unwrap_or_default();
         let state = serde_json::from_str(&saved_json).unwrap_or_else(|_| State::new());
 
-        Self {
+        let todo_app = Self {
             input_box,
             button,
             list,
             state,
-        }
+        };
+
+        let mut todo_app_clone = todo_app.clone();
+        EventListener::new(&todo_app.button, "click", move |_| {
+                let _ = todo_app_clone.add_task();
+        }).forget();
+
+        let todo_app_clone = todo_app.clone();
+        EventListener::new(&todo_app.list, "click", move |event| {
+            let target = event.target().unwrap().dyn_into::<HtmlElement>().unwrap();
+            if target.tag_name() == "LI" {
+                let _ = target.class_list().toggle_with_force("checked", true);
+                let _ = todo_app_clone.save_data();
+            } else if target.tag_name() == "SPAN" {
+                target.parent_element().unwrap().remove();
+                let _ = todo_app_clone.save_data();
+            }
+        }).forget();
+
+        todo_app
     }
 
-    fn add_task(&self) {
+    fn add_task(&mut self) {
         if self.input_box.value().is_empty() {
             let _ = alert("Please enter a task");
         } else {
-            let li = document().create_element("li").unwrap();
-            li.set_inner_html(&self.input_box.value());
-            self.list.append_child(&li).unwrap();
-            self.input_box.set_value("");
-            let span = document().create_element("span").unwrap();
-            span.set_inner_html("&times;");
-            li.append_child(&span).unwrap();
+            let task_name = &self.input_box.value();
+            self.state.add(task_name.clone());
+            let task = self.state.tasks.last().unwrap();
+            let task_html = self.create_task_html(task);
+            let _ = self.list.insert_adjacent_html("beforeend", &task_html);
+            self.save_data();
         }
-        self.save_data();
     }
 
     fn save_data(&self) {
@@ -67,7 +82,7 @@ impl TodoApp {
             <li class=\"{}\">
                 {}
                 <span>&times;</span>
-            </li>)
+            </li>
         ", if task.completed { "checked" } else { "" }, task.name)
     }
 
@@ -79,32 +94,8 @@ impl TodoApp {
     }
 }
 
-
 #[wasm_bindgen(main)]
 pub fn main() {
     log!("Welcome to the wasm world!");
-    let todo_app = Rc::new(TodoApp::new());
-
-    { 
-        let todo_app_clone = todo_app.clone();
-        EventListener::new(&todo_app.list, "click", move |event| {
-            let target = event.target().unwrap().dyn_into::<HtmlElement>().unwrap();
-            if target.tag_name() == "LI" {
-                let _ = target.class_list().toggle_with_force("checked", true);
-                let _ = todo_app_clone.save_data();
-            } else if target.tag_name() == "SPAN" {
-                target.parent_element().unwrap().remove();
-                let _ = todo_app_clone.save_data();
-            }
-        }).forget();
-    }
-
-    {
-        let todo_app_clone = todo_app.clone();
-        EventListener::new(&todo_app.button, "click", move |_| {
-            let _ = todo_app_clone.add_task();
-        }).forget();
-    }
-
-    todo_app.show_task();
+    TodoApp::new().show_task();
 }
